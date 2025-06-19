953fbf9eefaefb029c0f9e77c3b3c73f0a3dc278
enesflow <mail@enesin.xyz>
2025-06-19 13:41:38 +0300

    First commit

---
+++ scripts/content.js
@@ -0,0 +1,33 @@
+const FREQUENCY = 1 / 10;
+const TARGETS = [
+  "https://en.wikipedia.org/wiki/Special:Random", // Random Wikipedia article
+  "https://commons.wikimedia.org/wiki/Special:Random/File", // Random Wikimedia Commons file
+  "useless-web", // Random useless web site
+  "https://c.xkcd.com/random/comic/", // Random XKCD comic
+  "https://www.boredpanda.com/random/", // Random Bored Panda article
+  "https://randomstreetview.com/", // Random Street View
+];
+function getRandomTarget() {
+  let target = TARGETS[Math.floor(Math.random() * TARGETS.length)];
+  if (target === "useless-web") {
+    target = USELESS_SITES[Math.floor(Math.random() * USELESS_SITES.length)];
+  }
+  return target;
+}
+
+const links = document.querySelectorAll("a");
+const length = links.length;
+
+// select at most length / frequency links
+// first lets select indexes of links to select
+const indexes = [];
+for (let i = 0; i < length; i++) {
+  if (Math.random() < FREQUENCY) {
+    indexes.push(i);
+  }
+}
+for (let i = 0; i < indexes.length; i++) {
+  const link = links[indexes[i]];
+  const randomLink = getRandomTarget();
+  link.href = randomLink;
+}
